591d3b41a06aa890c59491453ecd7e20d4777611
xmarduel <xavier.marduel@web.de>
2022-09-24 11:31:29 +0200

    style merge

--- utilities/svgresolver/svgresolve.py
+++ utilities/svgresolver/svgresolve.py
@@ -23,6 +23,17 @@ class SvgResolver:
 
         # to use the right id in the <use> stuff
         self.id_mapping = {}
+        self.id_style = {}
+
+    def resolve(self):
+        '''
+        The main routine
+        '''
+        self.collect_shapes()
+        self.collect_id_mapping()
+    
+        # output
+        self.write_result()
 
     def collect_shapes(self):
         '''
@@ -39,212 +50,30 @@ class SvgResolver:
             elif isinstance(e, svgelements.Shape):
                 #print("shape")
                 self.shapes.append(e)
-            else:
-                try:
-                    if e.values["tag"] == svgelements.SVG_TAG_USE:
-                        print("... found a '<use>' tag !")
-                        id_ref = e.values["href"][1:] # remove the '#'
-                        print("        -> def id = %r ### %r" % (id_ref, e.id))
-                        self.id_mapping.setdefault(id_ref, []).append(e.id)
-                except Exception as e:
-                    pass
+
+    def collect_id_mapping(self):
+        '''
+        every <use ...> item  with an id is set into the mapping
+        '''
+        for e in self.svg.elements():
+            try:
+                if e.values["tag"] == svgelements.SVG_TAG_USE:
+                    print("... found a '<use>' tag !")
+                    id_ref = e.values["href"][1:] # remove the '#'
+                    print("        -> def id = %r ### %r" % (id_ref, e.id))
+                    self.id_mapping.setdefault(id_ref, []).append(e.id)
+                    if "style" in e.values:
+                        self.id_style.setdefault(id_ref, []).append(e.values["style"])
+                    else:
+                       self.id_style.setdefault(id_ref, []).append({})
+            except Exception as e:
+                pass
 
         print("ID MAPPING", self.id_mapping)
         
-    def extract_id(self, shape):
-        '''
-        if not in "id_mapping":
-            - return the id of the shape
-        else:
-            - return the id of the shape mapping and remove this value
-            from the mapping
-        '''
-        if shape.id in self.id_mapping:
-            # in <use> hopefully
-            return self.id_mapping[shape.id].pop(0)
-
-        # was not a <use> but a normal element
-        return shape.id
-
-    def make_xml_circle(self, parent, shape):
+    def write_result(self):
         '''
         '''
-        print("Circle!")
-
-        c = etree.SubElement(parent, "circle")
-
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            c.attrib["id"] = the_id
-
-        c.attrib["cx"] = self.lf_format % shape.cx
-        c.attrib["cy"] = self.lf_format % shape.cy
-        c.attrib["r"] = self.lf_format % shape.rx
-        c.attrib["style"] = shape.values["style"]
-        
-    def make_xml_rect(self, parent, shape):
-        '''
-        '''
-        print("Rect!")
-
-        r = etree.SubElement(parent, "rect")
-        
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            r.attrib["id"] = the_id
-
-        r.attrib["width"] = self.lf_format % shape.width
-        r.attrib["height"] = self.lf_format % shape.height
-        r.attrib["x"] = self.lf_format % shape.x
-        r.attrib["y"] = self.lf_format % shape.y
-        r.attrib["rx"] = self.lf_format % shape.rx
-        r.attrib["ry"] = self.lf_format % shape.ry
-
-        if "style" in shape.values:
-            r.attrib["style"] = shape.values["style"]
-
-    def make_xml_ellipse(self, parent, shape):
-        '''
-        '''
-        print("Ellipse!")
-
-        e = etree.SubElement(parent, "ellipse")
-
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            e.attrib["id"] = the_id
-
-        e.attrib["cx"] = self.lf_format % shape.cx
-        e.attrib["cy"] = self.lf_format % shape.cy
-        e.attrib["rx"] = self.lf_format % shape.rx
-        e.attrib["ry"] = self.lf_format % shape.ry
-
-        if "style" in shape.values:
-            e.attrib["style"] = shape.values["style"]
-
-    def make_xml_polygon(self, parent, shape):
-        '''
-        '''
-        print("Polygon!")
-
-        p = etree.SubElement(parent, "polygon")
-
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            p.attrib["id"] = the_id
-
-        p.attrib["points"] =  " ".join(["%.3f,%.3f" % (pt.x, pt.y) for pt in shape.points])
-            
-        if "style" in shape.values:
-            p.attrib["style"] = shape.values["style"]
-
-        if "stroke-width" in shape.values:
-            p.attrib["stroke-width"] = shape.values["stroke-width"]
-        if "stroke" in shape.values:
-            p.attrib["stroke"] = shape.values["stroke"]
-        if "fill" in shape.values:
-            p.attrib["fill"] = shape.values["fill"]
-        if "opacity" in shape.values:
-            p.attrib["opacity"] = shape.values["opacity"]
-
-    def make_xml_line(self, parent, shape):
-        '''
-        '''
-        print("SimpleLine!")
-
-        l = etree.SubElement(parent, "line")
-
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            l.attrib["id"] = the_id
-
-        l.attrib["x1"] = self.lf_format % shape.x1
-        l.attrib["y1"] = self.lf_format % shape.y1
-        l.attrib["x2"] = self.lf_format % shape.x2
-        l.attrib["y2"] = self.lf_format % shape.y2
-
-        if "style" in shape.values:
-            l.attrib["style"] = shape.values["style"]
-        if "stroke-width" in shape.values:
-            l.attrib["stroke-width"] = shape.values["stroke-width"]
-        if "stroke" in shape.values:
-            l.attrib["stroke"] = shape.values["stroke"]
-        if "fill" in shape.values:
-            l.attrib["fill"] = shape.values["fill"]
-
-    def make_xml_polyline(self, parent, shape):
-        '''
-        '''
-        print("Polyline!")
-
-        p = etree.SubElement(parent, "polyline")
-
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            p.attrib["id"] = the_id
-
-        p.attrib["points"] =  " ".join(["%.3f,%.3f" %(pt.x, pt.y) for pt in shape.points])
-            
-        if "style" in shape.values:
-            p.attrib["style"] = shape.values["style"]
-        if "fill" in shape.values:
-            p.attrib["fill"] = shape.values["fill"]
-        if "stroke" in shape.values:
-            p.attrib["stroke"] = shape.values["stroke"]
-
-    def make_xml_path(self, parent, shape):
-        '''
-        '''
-        print("Path!")
-
-        p = etree.SubElement(parent, "path")
-
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            p.attrib["id"] = the_id
-
-        p.attrib["d"] = shape.d()
-
-        if "style" in shape.values:
-            p.attrib["style"] = shape.values["style"]
-
-    def make_xml_text(self, parent, shape):
-        '''
-        BUG: svgelements : shape.x and shape.y are **NOT** calculated!
-        '''
-        print("Text!")
-
-        t = etree.SubElement(parent, "text")
-
-        the_id = self.extract_id(shape)
-
-        if the_id != None:
-            t.attrib["id"] = the_id
-
-        t.attrib["x"] = self.lf_format % shape.x  # bug!
-        t.attrib["y"] = self.lf_format % shape.y  # bug!
-
-        #print(shape)
-
-        t.text = shape.text
-
-        if "style" in shape.values:
-            t.attrib["style"] = shape.values["style"]
-
-    def resolve(self):
-        '''
-        '''
-        self.collect_shapes()
-    
-        # ---- lxml -----------------------------------------
-
         NSMAP={None : "http://www.w3.org/2000/svg", "xlink":"http://www.w3.org/1999/xlink", "svg":"http://www.w3.org/2000/svg"} 
  
         asvg = etree.Element("svg", nsmap=NSMAP)
@@ -255,6 +84,7 @@ class SvgResolver:
         asvg.attrib["version"] = self.svg.values["version"]
         asvg.attrib["id"] = self.svg.values["id"]
 
+        # into a flat list
         g = etree.SubElement(asvg, "g")
         g.attrib["id"] = "layer1"
 
@@ -291,6 +121,249 @@ class SvgResolver:
                 pretty_print=True, 
                 xml_declaration=True, 
                 encoding='utf-8')
+
+    def resolve_id(self, shape):
+        '''
+        if not in "id_mapping":
+            - return the id of the shape
+        else:
+            - return the id of the shape id mapping and remove this value
+            from the mapping because just used
+        '''
+        if shape.id in self.id_mapping:
+            # in <use> hopefully
+            return self.id_mapping[shape.id].pop(0)
+
+        # was not a <use> but a normal element
+        return shape.id
+
+    def resolve_style(self, shape):
+        '''
+        if not in "id_style":
+            - return the style of the shape
+        else:
+            - return the style of the shape id style and remove this value
+            from the mapping because just used
+        '''
+        if shape.id in self.id_style:
+            # in <use> hopefully
+            return self.id_style[shape.id].pop(0)
+
+        # was not a <use> but a normal element
+        return shape.style
+
+    def merge_styles(self, ref_style:str, style: str) -> str:
+        '''
+        '''
+        if len(style) == 0:
+            return ref_style
+
+        try:
+            the_ref_style = {}
+            the_style = {}
+
+            ref_style_items = ref_style.split(";")
+            for item in ref_style_items:
+                key, value = item.split(":")
+                the_ref_style[key] = value
+
+            style_items = style.split(";")
+            for item in style_items:
+                key, value = item.split(":")
+                the_style[key] = value
+
+            # merge dict
+            the_ref_style.update(the_style)
+
+            merge_style = []
+            for key in the_ref_style:
+                merge_style.append("%s:%s" % (key, the_ref_style[key]))
+     
+            return ";".join(merge_style)
+
+        except:
+            return ref_style 
+
+    def make_xml_circle(self, parent, shape):
+        '''
+        '''
+        print("Circle!")
+
+        c = etree.SubElement(parent, "circle")
+
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            c.attrib["id"] = the_id
+
+        c.attrib["cx"] = self.lf_format % shape.cx
+        c.attrib["cy"] = self.lf_format % shape.cy
+        c.attrib["r"] = self.lf_format % shape.rx
+
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            c.attrib["style"] = style
+        
+    def make_xml_rect(self, parent, shape):
+        '''
+        '''
+        print("Rect!")
+
+        r = etree.SubElement(parent, "rect")
+        
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            r.attrib["id"] = the_id
+
+        r.attrib["width"] = self.lf_format % shape.width
+        r.attrib["height"] = self.lf_format % shape.height
+        r.attrib["x"] = self.lf_format % shape.x
+        r.attrib["y"] = self.lf_format % shape.y
+        r.attrib["rx"] = self.lf_format % shape.rx
+        r.attrib["ry"] = self.lf_format % shape.ry
+
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            r.attrib["style"] = style
+
+    def make_xml_ellipse(self, parent, shape):
+        '''
+        '''
+        print("Ellipse!")
+
+        e = etree.SubElement(parent, "ellipse")
+
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            e.attrib["id"] = the_id
+
+        e.attrib["cx"] = self.lf_format % shape.cx
+        e.attrib["cy"] = self.lf_format % shape.cy
+        e.attrib["rx"] = self.lf_format % shape.rx
+        e.attrib["ry"] = self.lf_format % shape.ry
+
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            e.attrib["style"] = style
+
+    def make_xml_polygon(self, parent, shape):
+        '''
+        '''
+        print("Polygon!")
+
+        p = etree.SubElement(parent, "polygon")
+
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            p.attrib["id"] = the_id
+
+        p.attrib["points"] =  " ".join(["%.3f,%.3f" % (pt.x, pt.y) for pt in shape.points])
+            
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            p.attrib["style"] = style
+
+    def make_xml_line(self, parent, shape):
+        '''
+        '''
+        print("SimpleLine!")
+
+        l = etree.SubElement(parent, "line")
+
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            l.attrib["id"] = the_id
+
+        l.attrib["x1"] = self.lf_format % shape.x1
+        l.attrib["y1"] = self.lf_format % shape.y1
+        l.attrib["x2"] = self.lf_format % shape.x2
+        l.attrib["y2"] = self.lf_format % shape.y2
+
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            l.attrib["style"] = style
+
+    def make_xml_polyline(self, parent, shape):
+        '''
+        '''
+        print("Polyline!")
+
+        p = etree.SubElement(parent, "polyline")
+
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            p.attrib["id"] = the_id
+
+        p.attrib["points"] =  " ".join(["%.3f,%.3f" %(pt.x, pt.y) for pt in shape.points])
+            
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            p.attrib["style"] = style
+
+    def make_xml_path(self, parent, shape):
+        '''
+        '''
+        print("Path!")
+
+        p = etree.SubElement(parent, "path")
+
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            p.attrib["id"] = the_id
+
+        p.attrib["d"] = shape.d()
+
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            p.attrib["style"] = style
+
+    def make_xml_text(self, parent, shape):
+        '''
+        BUG: svgelements : shape.x and shape.y are **NOT** calculated!
+        '''
+        print("Text!")
+
+        t = etree.SubElement(parent, "text")
+
+        the_id = self.resolve_id(shape)
+        the_style = self.resolve_style(shape)
+
+        if the_id != None:
+            t.attrib["id"] = the_id
+
+        t.attrib["x"] = self.lf_format % shape.x  # bug!
+        t.attrib["y"] = self.lf_format % shape.y  # bug!
+
+        #print(shape)
+
+        t.text = shape.text
+
+        # merge style of ref item with used item
+        style = self.merge_styles(shape.values.get("style", {}), the_style)
+        if style:
+            t.attrib["style"] = style
+
+    
 
     
     
