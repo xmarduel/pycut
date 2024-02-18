cc6ac4f5a22eba49b7f25871df0b128d9411c9e1
xmarduel <xavier.marduel@web.de>
2024-02-18 11:21:33 +0100

    c

--- misc_private/spirale.py
+++ misc_private/spirale.py
@@ -7,12 +7,12 @@ pi = np.pi
 
 
 CUTTER_RADIUS = 3  # mm
-OVERLAP = 0.66666666  # [0:1] in ratio -> cut size = (1 - overlap)
+OVERLAP = 0.965  # [0:1] in ratio -> cut size = (1 - overlap)
 CUT_SIZE = CUTTER_RADIUS * (1 - OVERLAP)  # -> 1.0 mm
-CUT_SIZE = 1.0
+# CUT_SIZE = 1.0
 print("CUT_SIZE = ", CUT_SIZE)
 
-POCKET_RADIUS = 20  # mm
+POCKET_RADIUS = 15  # mm
 NB_CIRCLES = math.floor((POCKET_RADIUS - CUTTER_RADIUS) / CUT_SIZE)
 print("NB_CIRCLES = ", NB_CIRCLES)
 
@@ -34,6 +34,9 @@ NB_POINTS = math.floor(
     (2 * pi / SEGMENT_LEN) * (CUT_SIZE * NB_CIRCLES * NB_CIRCLES_P_1 / 2)
 )
 print("NB_POINTS = ", NB_POINTS)
+
+# NB_POINTS *= 2
+# print("NB_POINTS = ", NB_POINTS)
 
 POCKET_RADIUS_M_CUTTER = POCKET_RADIUS - CUTTER_RADIUS
 
@@ -116,6 +119,122 @@ class Spiral:
 
         plt.show()
 
+    def generate_g_code(self, ops):
+        """ """
+        clearance = 5.0
+        x_shift_after_op = 11.5
+
+        prolog = f"""
+G21         ; Set units to mm
+G90         ; Absolute positioning
+G1 {clearance}    F500      ; Move to clearance level
+
+; Start the spindle
+M3 S1000
+
+;
+; Tool Info
+; Diameter:    6.0
+
+"""
+
+        epilog = f"""
+; Retract
+G1 Z{clearance} F500
+
+; Stop the spindle
+M5
+
+; Return to 0,0
+G0 X0 Y0 F500
+
+; Program End
+M2
+"""
+
+        gcode = prolog
+
+        for k, op in enumerate(ops):
+
+            center = op["center"]
+            cut_depth = op["cut_depth"]
+            pass_depth = op["pass_depth"]
+
+            nb_levels = math.floor(cut_depth / pass_depth) + 1
+
+            levels = [0]
+            for k in range(nb_levels):
+                levels.append(-(k + 1) * pass_depth)
+            levels[-1] = -cut_depth
+
+            print("op levels:", levels)
+
+            op_prolog = f""";
+; Operation:    {k+1}
+; Name:         pocket_spiral
+; Type:         Pocket
+; Paths:        1
+; Direction:    Conventional
+; Cut Depth:    {cut_depth}
+; Pass Depth:   {pass_depth}
+; Plunge rate:  30
+; Cut rate:     250
+;
+"""
+
+            op_to_initial_position = f"""
+; Path 1
+; Rapid to initial position
+
+;
+;G1 X{center[0]} Y{center[1]} F500
+
+G1 X{x_shift_after_op} F500
+G1 Y{center[1]} F500
+G1 X{center[0]} F500
+
+G1 Z0.0
+"""
+
+            op_epilog = f"""
+; Retract
+G1 Z{clearance} F500
+
+; EXTRA: got right
+G1 X{x_shift_after_op}
+"""
+
+            spirale_lines = []
+
+            for k, level in enumerate(levels):
+
+                if k == 0:
+                    continue
+
+                spirale_lines.append(f"G1 Z{level} F25")
+                spirale_lines.append(f"G1 Z{level} F250")
+
+                for x, y in zip(self.x, self.y):
+                    xx = "%4.*f" % (4, x + center[0])
+                    yy = "%4.*f" % (4, y + center[1])
+
+                    spirale_lines.append(f"G1 X{xx} Y{yy}")
+
+                prev_level = levels[k - 1]
+                spirale_lines.append(f"G1 Z{prev_level}")
+
+                spirale_lines.append(f"G1 X{center[0]} Y{center[1]}")
+
+                spirale = "\n".join(spirale_lines)
+
+            gcode += op_prolog + op_to_initial_position + spirale + op_epilog
+
+        gcode += epilog
+
+        fp = open("gcode.nc", "w")
+        fp.write(gcode)
+        fp.close()
+
 
 class Square(Spiral):
 
@@ -186,7 +305,15 @@ class Square(Spiral):
 
 if __name__ == "__main__":
     spiral = Spiral()
-    spiral.plot()
+    # spiral.plot()
 
-    square = Square()
-    square.plot()
+    ops = [
+        {"center": [0.0, 0.0], "cut_depth": 25.0, "pass_depth": 10.0},
+        {"center": [0.0, 170.0], "cut_depth": 25.0, "pass_depth": 10.0},
+        {"center": [0.0, 85.0], "cut_depth": 17.0, "pass_depth": 10.0},
+    ]
+
+    spiral.generate_g_code(ops)
+
+    # square = Square()
+    # square.plot()
